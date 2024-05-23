14fa01922884ae5ed3f20c984a18716fbece2f56
Akhilvarma9999 <115726713+Akhilvarma9999@users.noreply.github.com>
2024-05-23 07:50:38 +0000

    added class to get data from IEX api

--- dashboard.py
+++ dashboard.py
@@ -1,9 +1,11 @@
 import streamlit as st
 import requests
 import config
-
+from iex import IEXStock
+from helper import format_number
 
 symbol = st.sidebar.text_input("Symbol",value="MSFT")
+stock=IEXStock(config.IEX_API_TOKEN,symbol)
 
 screen = st.sidebar.selectbox("view",('Overview','Fundamentals','News','Ownership','Technicals'))
 st.title(screen)
@@ -11,31 +13,50 @@ st.title(screen)
 
 
 if screen == "Overview":
-    url=f"https://api.iex.cloud/v1/stock/{symbol}/logo?token={config.IEX_API_TOKEN}"
-    r=requests.get(url)
-    logo=r.json()
+    logo =stock.get_logo()
+    company_info=stock.get_company_info()
+    print(company_info)
     
 
-    url=f"https://api.iex.cloud/v1/data/core/company/{symbol}?token={config.IEX_API_TOKEN}"
-    r=requests.get(url)
-    response_json=r.json()
-    
+    if company_info and isinstance(company_info, list) and len(company_info) > 0:
+    # Access the first item in the list
+        info = company_info[0]
 
-    col1 ,col2 =st.columns([1,4])
-    with col1:
-        st.image(logo['url'])
-    with col2:
-        
-        st.subheader("Description")
-        st.write(response_json['description'])
-        st.subheader("Industry")
-        st.write(response_json['industry'])
-        st.subheader("CEO")
-        st.write(response_json['CEO'])
+        col1, col2 = st.columns([1, 4])
+        with col1:
+            # Assuming 'logo' contains 'url' directly
+            if 'url' in logo:
+                st.image(logo['url'])
+            else:
+                st.error("Logo URL not found")
+        with col2:
+            st.subheader("Description")
+            st.write(info.get('longDescription', 'Description not available'))
+
+            st.subheader("Industry")
+            st.write(info.get('industry', 'Industry not available'))
+
+            st.subheader("CEO")
+            st.write(info.get('ceo', 'CEO not available'))
+    else:
+        st.error("company_info is not a list or it's empty")
 
 
     
   
 if screen == "Fundamentals":
-    pass
+    stats=stock.get_stats()
+    if stats and isinstance(stats, list):
+        # Find the dictionary that contains 'currentCash' and 'reportDate' keys
+        stats_info = stats[0] if stats and isinstance(stats[0], dict) else {}
+
+        st.subheader("CurrentCash")
+        st.write(format_number(stats_info.get("currentCash", "CurrentCash not available")))
+
+        st.subheader("ReportedDate")
+        st.write(stats_info.get("reportDate", "ReportedDate not available"))
+        st.write(stats_info)
+    else:
+        st.error("stats is not a list or it's empty")
+
     
--- iex.py
+++ iex.py
@@ -1,1 +1,20 @@
 import requests
+
+class IEXStock:
+    def __init__(self,token,symbol):
+        self.BASE_URL = 'https://api.iex.cloud/v1/data/core'
+        self.token = token
+        self.symbol = symbol
+    def get_logo(self):
+        url=f"https://api.iex.cloud/v1/stock/{self.symbol}/logo?token={self.token}"
+        r=requests.get(url)
+        return r.json()
+    def get_company_info(self):
+        url=f"{self.BASE_URL}/company/{self.symbol}?token={self.token}"
+        r=requests.get(url)
+        return r.json()
+    def get_stats(self):
+        url=f"{self.BASE_URL}/balance_sheet/{self.symbol}?token={self.token}"
+        r=requests.get(url)
+        return r.json()
+        
